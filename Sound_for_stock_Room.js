32a49ae9836a11a5a287141a1b3d5370f28022a7
Nicolas Borboën <ponsfrilus@gmail.com>
2023-05-25 18:55:33 +0200

    [feature] Trigger sound based on XHR returns (v0.6)
    The sounds are now triggered based on the URL used by AJAX to POST 
    content, and the `responseText` is used to play the sound when it's 
    needed.
    
    This commit also beautify some of the code (surch a single quote and 
    spaces) and remove some dead code.

--- Sound_for_stock_Room.js
+++ Sound_for_stock_Room.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sound for Stock Room
-// @version      0.5
 // @namespace    stockroom-kiosk
+// @version      0.6
 // @description  Play Sound for Jaggaer Stock Room web interface
 // @author       EPFL Durrer Laurent
 // @match        https://erm-dev.epfl.ch/stockroom-kiosk/app/*
@@ -12,100 +12,102 @@
 // @grant        none
 // ==/UserScript==
 
-
-
-// hide cancel button
-$('#footer').hide();
-
-//analyse when "enter" action
-$(document).keypress(
-  function(event){
-    if (event.which == '13') {
-//when modify quantity focus to article scan
-        if ((document.activeElement).id == "quantity")
-            {
-               document.getElementById("bc").focus();
-               event.preventDefault();
-                return;
-            }
-//prevent checkout without article scan
-        if (document.activeElement.id == "bc" && document.getElementById("bc").value == "")
-            {
-            event.preventDefault();
-            }
-    }
-});
-
-
-//if scan start with $$ (=çç) modif quantity
-$(document).on('keyup', 'input', function (e) {
-    if ($(this).val().startsWith("çç")) {
-        this.value="";
-        document.getElementById("quantity").value = "";
-        document.getElementById("quantity").focus();
-        return;
-    }
-});
-
-
-
-// permit to use two jquery, remove conflict if you have on the html site and
-// an other used on Tampermonkey script
-$.noConflict(true)(function($) {
 'use strict';
+$( document ).ready( async () => {
 
-// Test If Tampermonkey work correctly
-var html = 'Ceci a été ajouté par Tampermonkey !!!';
-console.log($);
+    console.log( `UserScript "Sound for Stock Room" loaded...` );
 
-//Song player function
-function songPlayer(songUrl){
-    var playerPage = document.createElement('audio');
-    playerPage.src = songUrl;
-    playerPage.preload = 'auto';
-    return playerPage.play();
-}
+    // hide cancel button
+    $( '#footer' ).hide();
 
-// Auto start-over after 10 seconds on page cahckeout
-$(document).ready(function() {
-    if (window.location.href.indexOf("checkout/showreceipt") > -1) {
-        setTimeout(
-            function ( )
-            {
-                window.location.href ='/stockroom-kiosk/app/secure/kiosk/?clr=true';
-            }, 10000 );
-    }
-});
+    // analyse when "enter" action
+    $( document ).keypress(
+        ( event ) => {
+            if ( event.which == '13') {
+                // when modify quantity focus to article scan
+                if ( ( document.activeElement ).id == 'quantity' ) {
+                    document.getElementById( 'bc' ).focus();
+                    event.preventDefault();
+                    return;
+                }
+                // prevent checkout without article scan
+                if ( document.activeElement.id == 'bc' && document.getElementById( 'bc' ).value == '' ) {
+                    event.preventDefault();
+                }
+            }
+        });
 
-// Run this function after an ajax is complete
-window.$( document ).ajaxComplete(function( event, request, settings ) {
-    //debugger;
-    //console.log("coucou");
-
-    // Test value and execute sound
-    var notrecognized = ($("div#notrecognized").length);
-    var notrecognizedbystockroom = ($("div#notrecognizedbystockroom").length);
-    var containeralreadyscanned = ($("div#containeralreadyscanned").length);
-
-        if (notrecognized == 1){
-        songPlayer('https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/employee_bar_code_not_recognized.ogg');
-        console.log("Employee bar code not recognized");
-        //alert("Employee bar code not recognized");
+    // if scan start with $$ (=çç) modif quantity
+    $( document ).on( 'keyup', 'input', function (e) {
+        if ( $(this).val().startsWith( 'çç' ) ) {
+            this.value = '';
+            document.getElementById( 'quantity' ).value = '';
+            document.getElementById( 'quantity' ).focus();
+            return;
         }
-        if (notrecognizedbystockroom == 1){
-        songPlayer('https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/Item_not_recognized_by_stockroom.ogg');
-        console.log("Item not recognized by stockroom");
-        //alert("Item not recognized by stockroom");
-        }
-        if (containeralreadyscanned == 1){
-        songPlayer('https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/Container_has_been_scanned_already.ogg');
-        console.log("Container has been scanned already");
-        //alert("Container has been scanned already");
-        }
-});
+    });
 
-// Test for debugging
-////songPlayer('https://notificationsounds.com/soundfiles/3cf166c6b73f030b4f67eeaeba301103/file-sounds-902-oh-boy.ogg');
-////$('#header').append(html);
+    // permit to use two jquery, remove conflict if you have on the html site and
+    // an other used on Tampermonkey script
+    $.noConflict(true)( ($) => {
 
+        // Song player function
+        function songPlayer( songUrl ) {
+            var playerPage = document.createElement( 'audio' );
+            playerPage.src = songUrl;
+            playerPage.preload = 'auto';
+            return playerPage.play();
+        }
+
+        // Auto start-over after 10 seconds on page checkout
+        $( document ).ready( function() {
+            if ( window.location.href.indexOf( 'checkout/showreceipt' ) > -1 ) {
+                setTimeout(
+                    function () {
+                        window.location.href = '/stockroom-kiosk/app/secure/kiosk/?clr=true';
+                    }, 10000
+                );
+            }
+        });
+
+        // Run this function after an ajax is complete
+        window.$( document ).ajaxComplete( ( event, request, settings ) => {
+
+            let requestContent;
+            // Intercept "ajaxComplete" requests to play sounds
+            switch ( settings.url ) {
+
+                // Called at the /stockroom-kiosk/app/secure/kiosk/, "login" process
+                case '/stockroom-kiosk/app/secure/kiosk/validate/barcode' :
+                    requestContent = JSON.parse( request.responseText ).status
+                    if ( requestContent === -2 ) {
+                        songPlayer( 'https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/employee_bar_code_not_recognized.ogg' );
+                        console.log( `  ↳ Employee barcode not recognized` );
+                    }
+                    break;
+
+                // Called at the /stockroom-kiosk/app/secure/scan/, "scanning" process
+                case '/stockroom-kiosk/app/secure/scan/barcode' :
+                    requestContent = request.responseText.includes( 'id="notrecognizedbystockroom"' )
+                    if ( requestContent ) {
+                        songPlayer( 'https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/Item_not_recognized_by_stockroom.ogg' );
+                        console.log( `  ↳ Item not recognized by stockroom` );
+                    }
+                    break;
+
+                default:
+                    // Nothing to do with this URL... console.debug(settings.url)
+                    break;
+            }
+
+            // TODO: how to get there?
+            // Test value and execute sound
+            var containeralreadyscanned = ( $( 'div#containeralreadyscanned' ).length );
+            if ( containeralreadyscanned == 1 ) {
+                songPlayer( 'https://raw.githubusercontent.com/epfl-si/TamperMonkey_Jaggaer_Kiosk/master/Code/Container_has_been_scanned_already.ogg' );
+                console.log( `  ↳ Container has been scanned already` );
+            }
+
+        });
+    });
 });
